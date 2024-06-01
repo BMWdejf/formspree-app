9c7b74f5a7f96899ca8942601985e80ecc2efc06
BMWdejf <sablatura.david@gmail.com>
2024-06-01 17:35:20 +0200

    New test get data from Axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import ContactForm from './ContactForm';
+import './App.css';
+import ProjectList from './ProjectList';
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <ContactForm />
+    <div className="App">
+      <header className="App-header">
+        <h1>Project List</h1>
+      </header>
+      <ProjectList />
     </div>
   );
 }
+
+export default App;
