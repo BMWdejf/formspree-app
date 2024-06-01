755cb9b88663f5b1283ee9017b3e07676ac7a0c5
BMWdejf <sablatura.david@gmail.com>
2024-06-01 22:35:12 +0200

    Test 1 GraphQL

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
-import React from 'react';
-import './App.css';
-import ProjectList from './ProjectList';
+import React, { useEffect, useState } from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery } from '@apollo/client';
 
-function App() {
+const client = new ApolloClient({
+  uri: 'https://django-tutorial-one.vercel.app/projects/', // Zde vložte URL vašeho GraphQL serveru
+  cache: new InMemoryCache()
+});
+
+const GET_PROJECTS = gql`
+  query GetProjects {
+    projects {
+      name
+      start_date
+      end_date
+      comments
+      status
+    }
+  }
+`;
+
+const Projects = () => {
+  const { loading, error, data } = useQuery(GET_PROJECTS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Project List</h1>
-      </header>
-      <ProjectList />
+    <div>
+      {data.projects.map((project) => (
+        <div key={project.name}>
+          <h3>{project.name}</h3>
+          <p>Start Date: {project.start_date}</p>
+          <p>End Date: {project.end_date}</p>
+          <p>Comments: {project.comments}</p>
+          <p>Status: {project.status}</p>
+        </div>
+      ))}
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <h2>Projects List</h2>
+        <Projects />
+      </div>
+    </ApolloProvider>
+  );
+};
 
 export default App;
